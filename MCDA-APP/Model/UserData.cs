20cd88b52644f9c215e73b7b33244cb702fecb86
miltinhoc <miltinhoc@users.noreply.github.com>
2024-01-16 19:50:57 -0100

    created namespace for models
    added userdata class to the model namespace

---
+++ MCDA-APP/Model/UserData.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace MCDA_APP.Model
+{
+    internal class UserData
+    {
+        [JsonProperty("email")]
+        public string? Email { get; set; }
+
+        [JsonProperty("password")]
+        public string? Password { get; set; }
+    }
+}
