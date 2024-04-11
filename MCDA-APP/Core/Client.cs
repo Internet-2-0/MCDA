a3b1abc50797e09aa195ed7aa8ef63064fc98e0c
miltinhoc <miltonamc@gmail.com>
2024-04-11 18:49:04 +0000

    added jsonConverter to parse login response

--- MCDA-APP/Core/Client.cs
+++ MCDA-APP/Core/Client.cs
@@ -1,6 +1,11 @@
-﻿using MCDA_APP.Model.Agent;
+﻿using MCDA_APP.Json;
+using MCDA_APP.Model;
+using MCDA_APP.Model.Agent;
+using MCDA_APP.Model.Api;
 using MCDA_APP.Web;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace MCDA_APP.Core
 {
@@ -16,23 +21,34 @@ namespace MCDA_APP.Core
 
         private void SetupHeaders()
         {
-            if (_customHttpClient is null)
-            {
-                return;
-            }
-
             _customHttpClient.AddHeader("apiKey", "");
             _customHttpClient.AddHeader("source", "agent");
             _customHttpClient.AddHeader("agentVersion", Helper.GetAgentVersion());
         }
 
+        public async Task Login(string email, string password)
+        {
+            try
+            {
+                string response = await _customHttpClient.SendJsonRequestAsync($"{Constants.ApiBaseUrl}/auth/login", HttpMethod.Post, 
+                    new UserData() { Email = email, Password = password });
+
+                var settings = new JsonSerializerSettings
+                {
+                    Converters = new List<JsonConverter> { new AuthResponseConverter() },
+                };
+
+                AccountInformation? root = JsonConvert.DeserializeObject<AccountInformation>(response, settings);
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
         public async Task SendAgentStatus()
         {
-            if (_customHttpClient is null)
-            {
-                return;
-            }
-
             AgentStatus agentStatus = new()
             {
                 Payload = new Payload{ Message = "Agent Started" },
---
+++ MCDA-APP/Json/AuthResponseConverter.cs
@@ -0,0 +1,18 @@
+﻿using MCDA_APP.Model.Api;
+using Newtonsoft.Json;
+
+namespace MCDA_APP.Json
+{
+    public class AuthResponseConverter : JsonConverter<AccountInformation>
+    {
+        public override AccountInformation? ReadJson(JsonReader reader, Type objectType, AccountInformation? existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, AccountInformation? value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
